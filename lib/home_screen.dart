257b2d21979ba2e49e719f5321f4bbfb6f042790
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-10-27 00:50:01 +0500

    Detect Multiple Gestures

---
+++ lib/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(body:  Column(
+      children: [
+       InkWell(
+        onTap: (){
+          print("object");
+        },
+        onDoubleTap: (){
+          print("on double tap");
+
+        },
+        onHover: (value) {
+          print("Hover");
+        },
+        onLongPress: (){
+          print("Long Press");
+        },
+         child: Container(
+          height: 50,width: double.infinity,
+          color: Colors.blue,
+         ),
+       ),
+      ],
+    ));
+  }
+}
