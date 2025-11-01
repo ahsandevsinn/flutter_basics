d7c2b88a214f72df6a5a7d34810ae233d98da9ff
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-11-01 11:07:44 +0500

    material widgets vs cupertino widgets

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,42 +13,53 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+//     return Scaffold(body:  Column(
+//       children: [
+//        InkWell(
+//         onTap: (){
+//           print("object");
+//         },
+//         onDoubleTap: (){
+//           print("on double tap");
 
+//         },
+//         onHover: (value) {
+//           print("Hover");
+//         },
+//         onLongPress: (){
+//           print("Long Press");
+//         },
+//          child: Container(
+//           height: 50,width: double.infinity,
+//           color: Colors.blue,
+//          ),
+//        ),
+//        Dismissible(
+//   key: ValueKey('card1'),
+//   background: Container(color: Colors.red, child: Icon(Icons.delete)),
+//   onDismissed: (_) => print("Item Deleted"),
+//   child: Card(
+//     child: ListTile(
+//       title: Text("Swipe to delete me"),
+//       leading: Icon(Icons.touch_app),
+//     ),
+//   ),
+// )
 
-    return Scaffold(body:  Column(
-      children: [
-       InkWell(
-        onTap: (){
-          print("object");
-        },
-        onDoubleTap: (){
-          print("on double tap");
-
-        },
-        onHover: (value) {
-          print("Hover");
-        },
-        onLongPress: (){
-          print("Long Press");
-        },
-         child: Container(
-          height: 50,width: double.infinity,
-          color: Colors.blue,
-         ),
-       ),
-       Dismissible(
-  key: ValueKey('card1'),
-  background: Container(color: Colors.red, child: Icon(Icons.delete)),
-  onDismissed: (_) => print("Item Deleted"),
-  child: Card(
-    child: ListTile(
-      title: Text("Swipe to delete me"),
-      leading: Icon(Icons.touch_app),
-    ),
-  ),
-)
-
-      ],
-    ));
+//       ],
+//     ));
+//   }
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            child: Center(
+            child: CupertinoButton(
+                child: Icon(CupertinoIcons.add), onPressed: () {}),
+          ))
+        : Scaffold(
+            appBar: AppBar(),
+            body: Center(
+              child: ElevatedButton(onPressed: () {}, child: Icon(Icons.add)),
+            ),
+          );
   }
 }
