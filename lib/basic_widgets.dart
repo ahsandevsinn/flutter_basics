e6acac8836c78cd60f06679d4c8bb1eec624ac14
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-11-01 15:40:22 +0500

    Row

--- lib/basic_widgets.dart
+++ lib/basic_widgets.dart
@@ -15,6 +15,16 @@ class _BasicWidgetsState extends State<BasicWidgets> {
         children: [
           Text("Hello World"),
           Text("Hello World"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Hello World"),
+              SizedBox(
+                width: 10,
+              ),
+              Text("Hello World"),
+            ],
+          )
         ],
       ),
     );
